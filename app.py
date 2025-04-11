0ecd35554713d67543a4603dab66c32a12f3c16a
ashish12041993 <ashish.mnnit777@gmail.com>
2025-04-11 20:25:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import boto3
+
+    ec2 = boto3.client('ec2', region_name='us-east-1')  
+
+        response = ec2.run_instances(
+            ImageId='ami-0abcdef1234567890',  
+            InstanceType='t2.micro',
+            KeyName='your-key-pair-name',    
+            MinCount=1,
+            MaxCount=1,
